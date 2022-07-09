11f916d0f0182eaf5e87eb08831fc376d5e0a33c
OlegBSTU <polybox45@gmail.com>
2022-07-09 17:15:59 +0300

    test website

---
+++ website/script/main.js
@@ -0,0 +1,5 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+document.querySelector('html').addEventListener('click', function() {
+  alert('Ouch! Stop poking me!');
+});
